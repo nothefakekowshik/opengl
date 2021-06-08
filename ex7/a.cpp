c952636f9d365e5913120d25b7959c5811ccfa44
psrsai001@gmail.com <psrsai001@gmail.com>
2021-06-08 15:25:42 +0530

    added indentation on ex7

--- ex7/a.cpp
+++ ex7/a.cpp
@@ -23,59 +23,59 @@ void CohenSutherlandLineClipAndDraw (double x0, double y0,double x1, double y1)
     {
         if (!(outcode0 | outcode1)) //logical or is 0 Trivially accept & exit
         {
-        accept = true;
-        done = true;
+            accept = true;
+            done = true;
         }
         else if (outcode0 & outcode1) //logical and is not 0. Trivially reject and exit
-        done = true;
+            done = true;
         else
         {
-        //failed both tests, so calculate the line segment to clip
-        //from an outside point to an intersection with clip edge
-        double x, y;  
+            //failed both tests, so calculate the line segment to clip
+            //from an outside point to an intersection with clip edge
+            double x, y;  
 
 
 
-        //At least one endpoint is outside the clip rectangle; pick it.
-        outcodeOut = outcode0? outcode0: outcode1;
-        //Now find the intersection point;
-        //use formulas y = y0 + slope * (x - x0), x = x0+ (1/slope)* (y - y0)
-        if (outcodeOut & TOP) //point is above the clip rectangle
-        {
-        x = x0 + (x1 - x0) * (ymax - y0)/(y1 - y0);
-        y = ymax;
-        }
-        else if (outcodeOut & BOTTOM) //point is below the clip rectangle
-        {
-        x = x0 + (x1 - x0) * (ymin - y0)/(y1 - y0);
-        y = ymin;
-        }
-        else if (outcodeOut & RIGHT) //point is to the right of clip rectangle
-        {
-        y = y0 + (y1 - y0) * (xmax - x0)/(x1 - x0);
-        x = xmax;
-        }
-        else //point is to the left of clip rectangle
-        {
-        y = y0 + (y1 - y0) * (xmin - x0)/(x1 - x0);
-        x = xmin;
-        }
-        //Now we move outside point to intersection point to clip
-        //and get ready for next pass.
-        if (outcodeOut == outcode0)
+            //At least one endpoint is outside the clip rectangle; pick it.
+            outcodeOut = outcode0? outcode0: outcode1;
+            //Now find the intersection point;
+            //use formulas y = y0 + slope * (x - x0), x = x0+ (1/slope)* (y - y0)
+            if (outcodeOut & TOP) //point is above the clip rectangle
             {
-            x0 = x;
-            y0 = y;
-            outcode0 = ComputeOutCode (x0, y0);
+                x = x0 + (x1 - x0) * (ymax - y0)/(y1 - y0);
+                y = ymax;
             }
-        else
+            else if (outcodeOut & BOTTOM) //point is below the clip rectangle
             {
-            x1 = x;
-            y1 = y;
-            outcode1 = ComputeOutCode (x1, y1);
+                x = x0 + (x1 - x0) * (ymin - y0)/(y1 - y0);
+                y = ymin;
             }
+            else if (outcodeOut & RIGHT) //point is to the right of clip rectangle
+            {
+                y = y0 + (y1 - y0) * (xmax - x0)/(x1 - x0);
+                x = xmax;
+            }
+            else //point is to the left of clip rectangle
+            {
+                y = y0 + (y1 - y0) * (xmin - x0)/(x1 - x0);
+                x = xmin;
+            }
+            //Now we move outside point to intersection point to clip
+            //and get ready for next pass.
+            if (outcodeOut == outcode0)
+                {
+                    x0 = x;
+                    y0 = y;
+                    outcode0 = ComputeOutCode (x0, y0);
+                }
+            else
+                {
+                    x1 = x;
+                    y1 = y;
+                    outcode1 = ComputeOutCode (x1, y1);
+                }
         }
-        }while (!done);
+    }while (!done);
 
 
 
@@ -94,15 +94,15 @@ void CohenSutherlandLineClipAndDraw (double x0, double y0,double x1, double y1)
         //draw a red colored viewport
         glColor3f(1.0, 0.0, 0.0);
         glBegin(GL_LINE_LOOP);
-        glVertex2f(xvmin, yvmin);
-        glVertex2f(xvmax, yvmin);
-        glVertex2f(xvmax, yvmax);
-        glVertex2f(xvmin, yvmax);
+            glVertex2f(xvmin, yvmin);
+            glVertex2f(xvmax, yvmin);
+            glVertex2f(xvmax, yvmax);
+            glVertex2f(xvmin, yvmax);
         glEnd();
         glColor3f(0.0,0.0,1.0); // draw blue colored clipped line
         glBegin(GL_LINES);
-        glVertex2d (vx0, vy0);
-        glVertex2d (vx1, vy1);
+            glVertex2d (vx0, vy0);
+            glVertex2d (vx1, vy1);
         glEnd();
     }
 }
@@ -111,16 +111,16 @@ void CohenSutherlandLineClipAndDraw (double x0, double y0,double x1, double y1)
 
 outcode ComputeOutCode (double x, double y)
 {
-outcode code = 0;
-if (y > ymax) //above the clip window
-code |= TOP;
-else if (y < ymin) //below the clip window
-code |= BOTTOM;
-if (x > xmax) //to the right of clip window
-code |= RIGHT;
-else if (x < xmin) //to the left of clip window
-code |= LEFT;
-return code;
+    outcode code = 0;
+    if (y > ymax) //above the clip window
+    code |= TOP;
+    else if (y < ymin) //below the clip window
+    code |= BOTTOM;
+    if (x > xmax) //to the right of clip window
+    code |= RIGHT;
+    else if (x < xmin) //to the left of clip window
+    code |= LEFT;
+    return code;
 }
 
 
@@ -129,30 +129,30 @@ return code;
 
 void display()
 {
-double x0=120,y0=10,x1=40,y1=130;
-glClear(GL_COLOR_BUFFER_BIT);
+    double x0=120,y0=10,x1=40,y1=130;
+    glClear(GL_COLOR_BUFFER_BIT);
 
-//draw the line with red color
-glColor3f(1.0,0.0,0.0);
-//bres(120,20,340,250);
-glBegin(GL_LINES);
-glVertex2d (x0, y0);
-glVertex2d (x1, y1);
-glVertex2d (60,20);
-glVertex2d (80,120);
-glEnd();
+    //draw the line with red color
+    glColor3f(1.0,0.0,0.0);
+    //bres(120,20,340,250);
+    glBegin(GL_LINES);
+    glVertex2d (x0, y0);
+    glVertex2d (x1, y1);
+    glVertex2d (60,20);
+    glVertex2d (80,120);
+    glEnd();
 
-//draw a blue colored window
-glColor3f(0.0, 0.0, 1.0);
-glBegin(GL_LINE_LOOP);
-glVertex2f(xmin, ymin);
-glVertex2f(xmax, ymin);
-glVertex2f(xmax, ymax);
-glVertex2f(xmin, ymax);
-glEnd();
-CohenSutherlandLineClipAndDraw(x0,y0,x1,y1);
-CohenSutherlandLineClipAndDraw(60,20,80,120);
-glFlush();
+    //draw a blue colored window
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(xmin, ymin);
+    glVertex2f(xmax, ymin);
+    glVertex2f(xmax, ymax);
+    glVertex2f(xmin, ymax);
+    glEnd();
+    CohenSutherlandLineClipAndDraw(x0,y0,x1,y1);
+    CohenSutherlandLineClipAndDraw(60,20,80,120);
+    glFlush();
 }
 void myinit()
 {
